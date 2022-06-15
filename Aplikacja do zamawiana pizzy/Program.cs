44dbc123cf6abb9a789acdb7b30f7b7daf0fd554
Marcin Torbicz <marcintorbicz123@gmail.com>
2022-06-15 16:33:13 +0200

    Dodaj pliki projektów.

---
+++ Aplikacja do zamawiana pizzy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplikacja_do_zamawiana_pizzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
